6d293315810ac97e9b33764ac1c12be9102af1ae
angryhorse <groznykon@gmail.com>
2018-02-25 12:53:54 +0200

    badaboom

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-"use strict"
+$("#toggle").click(function() {
+  $(this).toggleClass('on');
+  $('#resize').toggleClass('active');
+});
+
+$('.hero').waypoint(function(direction) {
+  if (direction == "down") {
+    $('.nazgul').addClass('sticky-menu');
+    $('#toggle').addClass('tog-move')
+  } else if (direction == "up") {
+    $('.nazgul').removeClass('sticky-menu');
+    $('#toggle').removeClass('tog-move')
+  }
+});
+
+$('.container').waypoint(function(direction) {
+  if (direction == "down") {
+    $('.up-btn').addClass('sticky-up');
+  } else if (direction == "up") {
+    $('.up-btn').removeClass('sticky-up');
+  }
+});
+
+$('.right').waypoint(function(direction) {
+  if (direction == "down") {
+    $('#user-name').focus();
+  } else if (direction == "up") {
+    $(this).val('');
+    $(this).trigger('blur');
+  }
+});
